9a328b6c61cc06b022c79790e5647c9b93915f72
guidafe <136006478+guidafe@users.noreply.github.com>
2023-06-08 18:55:15 -0300

    [GDF 001] Adding react base builded

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bem vindo ao Guia da Fé. Estamos chegando em Aparecida do Norte!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
